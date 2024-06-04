ca370ceed7a98eaab210cd4c89456017c73551e1
yunhsuan0510 <166582621+yunhsuan0510@users.noreply.github.com>
2024-06-04 21:48:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,3 @@
-from flask import Flask, request, abort
-import os
-from pymongo import MongoClient
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
-
-app = Flask(__name__)
-
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-# 全局變數來保存用戶的區域選擇
-user_region = {}
-
-# 定義台中市區域列表
-taichung_regions = [
-    '南區', '北區', '中區', '西區', '東區', '北屯區', '大里區', '烏日區',
-    '南屯區', '西屯區', '大雅區', '豐原區', '潭子區'
-]
-
-def create_quick_reply_buttons():
-    items = [
-        QuickReplyButton(action=PostbackAction(label=region, data=f'region={region}'))
-        for region in taichung_regions
-    ]
-    return QuickReply(items=items)
-
-# 連接到 MongoDB Atlas
-def get_database(dbname):
-    CONNECTION_STRING = "mongodb+srv://r0980040:nuToa9PunCm65tgH@cluster0.wpk1rjx.mongodb.net/traveling"
-    client = MongoClient(CONNECTION_STRING)
-    return client[dbname]
-
-# 從資料庫中獲取隨機的項目
-def get_random_items_from_db(category, region):
-    db = get_database(category)
-    collection = db[region]
-    random_items = collection.aggregate([{'$sample': {'size': 3}}])
-    return list(random_items)
-
 def create_flex_message(data):
     bubbles = []
     for item in data:
@@ -65,9 +6,16 @@ def create_flex_message(data):
         address = item.get("Address", "無地址")
         business_hours = item.get("Business Hours", "無營業時間")
         google_maps_link = item.get("Google Maps Link", "https://maps.google.com")
-        
+        image_link = item.get("Image Link", "")
+
         bubble = BubbleContainer(
             direction='ltr',
+            hero=ImageComponent(
+                url=image_link,
+                size='full',
+                aspect_ratio='16:9',
+                aspect_mode='cover'
+            ),
             body=BoxComponent(
                 layout='vertical',
                 contents=[
@@ -88,55 +36,3 @@ def create_flex_message(data):
         bubbles.append(bubble)
 
     return FlexSendMessage(alt_text="資料庫查詢結果", contents=CarouselContainer(contents=bubbles))
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_id = event.source.user_id
-    user_input = event.message.text
-
-    if user_input == "開始":
-        reply_message = TextSendMessage(
-            text='請選擇您的所在區域',
-            quick_reply=create_quick_reply_buttons()
-        )
-        line_bot_api.reply_message(event.reply_token, reply_message)
-    elif user_input in ["美食", "點心", "景點"]:
-        region = user_region.get(user_id)
-        if region:
-            # 從資料庫中獲取資料
-            items = get_random_items_from_db(user_input, region)
-            reply_message = create_flex_message(items)
-            line_bot_api.reply_message(event.reply_token, reply_message)
-        else:
-            reply_message = TextSendMessage(text="請先選擇您的所在區域")
-            line_bot_api.reply_message(event.reply_token, reply_message)
-    else:
-        reply_message = TextSendMessage(text="請輸入 '開始' 來選擇您的所在區域")
-        line_bot_api.reply_message(event.reply_token, reply_message)
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    data = event.postback.data
-    user_id = event.source.user_id
-
-    if data.startswith('region='):
-        region = data.split('=')[1]
-        user_region[user_id] = region
-
-        reply_message = TemplateSendMessage(
-            alt_text='請選擇類別',
-            template=ButtonsTemplate(
-                title='請選擇服務項目',
-                text='請選擇您要找的是美食、點心還是景點',
-                actions=[
-                    MessageAction(label='美食', text='美食'),
-                    MessageAction(label='點心', text='點心'),
-                    MessageAction(label='景點', text='景點')
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, reply_message)
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
